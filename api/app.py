6b0b2e8590ed1c70032354fa3ddebd9d14cc51fd
yoon <llsee01@naver.com>
2024-10-24 16:43:13 +0900

    get방식 통신 api 추가

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 # 메인 플라스크 어플리케이션
 from flask import Flask, jsonify, request
+import urllib.parse
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +14,19 @@ def receive_data():
     
     # 간단한 응답
     response = {'status': 'success!!', 'received': data}
+    return jsonify(response)
+
+# GET 요청 처리
+@app.route('/receiveTestGetData', methods=['GET'])
+def receive_get_data():
+    # GET 요청으로 URL에서 'message' 파라미터를 받음
+    message = request.args.get('message')
+    # 인코딩된 데이터를 디코딩할 필요가 있음
+    decoded_message = urllib.parse.unquote(message) # URL 인코딩 해제
+    print(f"Flask 서버에서 데이터 받음: {decoded_message}")
+    
+    # 응답 반환
+    response = {'status': '성공함!', 'received_data': decoded_message}
     return jsonify(response)
 
 if __name__ == '__main__':  
